0c2d5bee6cb70512eb75041a4e390d6715c9d36a
Javier Azua <azua.javi@gmail.com>
2022-12-15 16:59:09 -0300

    gitignore

--- tech-ecomm/sanity.cli.ts
+++ tech-ecomm/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'eef2fkii',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
