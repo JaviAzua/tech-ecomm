7e67e179b9f50ae790e14d51ea07ec847550c350
Javier Azua <azua.javi@gmail.com>
2022-12-16 15:06:50 -0300

    Banner Done

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -18,17 +18,20 @@ function HeroBanner({ bannerData }: Props) {
         className="absolute inset-0 rounded-2xl object-cover opacity-60 group-hover:opacity-100 transition-all duration-300"
         fill
       />
-      <h2 className="absolute text-white/20 font-extrabold text-8xl lg:text-9xl right-0">
+      <h2 className="absolute text-white/20 font-extrabold text-8xl md:text-9xl left-2 md:inset-x-0 md:top-0 lg:text-right ">
         {bannerData?.midText.toUpperCase()}
       </h2>
-      <h3 className="text-white font-black text-5xl md:text-6xl lg:text-7xl absolute top-1/3 left-2">
-        {bannerData?.smallText.toUpperCase()}
-      </h3>
-      <div className=" w-3/4 md:w-full relative h-full">
-        <button className="text-white font-semibold absolute py-5 px-20 md:px-32 w-auto bg-tightRed bottom-1/3 left-2 lg:bottom-1/5 lg:left-10 hover:bg-lightRed hover:text-lightgrey duration-700 ">
+
+      <div className="absolute left-4 top-1/3 md:top-2/4 lg:top-1/3">
+        <h3 className="text-white font-black text-5xl md:text-6xl lg:text-7xl ">
+          {bannerData?.smallText.toUpperCase()}
+        </h3>
+        <button className="text-white font-semibold py-8 px-20 md:py-10 lg:py-6 md:px-32 lg:px-44 bg-tightRed hover:bg-lightRed hover:text-lightgrey duration-700 ">
           {bannerData?.buttonText}
         </button>
       </div>
+
+      <div className=" w-3/4 md:w-full relative h-full"></div>
       <div className="absolute bottom-0 left-2 lg:left-3/4">
         <p className=" text-tightRed">
           {bannerData?.product} - Save up to {bannerData?.discount} %
